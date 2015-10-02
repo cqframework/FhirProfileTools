0a0b0d0a3a4601e01433cd229d1b5d40ea5f7335
Jason Mathews <mathews@mitre.org>
2015-10-02 08:20:19 -0400

    add additional assertions to tests for more code coverage

--- src/test/groovy/TestFhirSimpleBase.groovy
+++ src/test/groovy/TestFhirSimpleBase.groovy
@@ -18,19 +18,36 @@ class TestFhirSimpleBase extends FhirSimpleBase {
     def profile = (StructureDefinition)parseResource(new File(publishDir, 'qicore-referralrequest.profile.xml'))
     assert profile != null
 
-    def elt = getElementByName(profile.getDifferential().getElement(), 'ReferralRequest.requester', null)
-    assert elt && elt.getMustSupport()
+    // find element by its path in snapshot
+    def elt = getElementByName(profile.getSnapshot().getElement(), 'ReferralRequest.status', null)
+    assert elt != null && elt.getPath() == 'ReferralRequest.status'
+    def shortList = typeShortList(elt.getType(), false)
+    assert shortList.size() == 1 && shortList.get(0) == 'code'
+
+    // find element by its path in differential
+    elt = getElementByName(profile.getDifferential().getElement(), 'ReferralRequest.requester', null)
+    assert elt != null && elt.getMustSupport()
     // [Reference(qicore-practitioner | qicore-organization | qicore-patient
-    def shortList = typeShortList(elt.getType())
+    shortList = typeShortList(elt.getType())
     assert shortList.size() == 1 && shortList.get(0).startsWith('Reference(')
     shortList = typeShortList(elt.getType(), false)
-	assert shortList.size() == 1 && shortList.get(0) == 'qicore-practitioner | qicore-organization | qicore-patient'
+    assert shortList.size() == 1 && shortList.get(0) == 'qicore-practitioner | qicore-organization | qicore-patient'
 
+    def typeSet = getTypeSet(elt)
+    //[Reference(qicore-organization), Reference(qicore-patient), Reference(qicore-practitioner)]
+    assert typeSet.size() == 3
+
+    elt = getElementByName(profile.getSnapshot().getElement(), elt)
+    assert elt != null && elt.getPath() == 'ReferralRequest.requester'
+
+    // find extension by its name in differential
     elt = getExtensionByName(profile.getDifferential().getElement(), 'ReferralRequest.refusalReason')
-    assert elt != null
+    assert elt != null && elt.getPath() == 'ReferralRequest.extension'
+
     // println "path=" + elt.getPath()
     def type = elt.hasType() ? elt.getType().get(0) : null
     assert type && type.getCode() == 'Extension'
+    assert getReference(elt.getBinding()) == 'http://hl7.org/fhir/ValueSet/v3-ActReason'
     String extProfile = getProfile(type)
     // println "profile=$extProfile"
     def extProfileDef = createExtensionDef(elt, extProfile)
@@ -56,6 +73,7 @@ class TestFhirSimpleBase extends FhirSimpleBase {
       }
       profile.getDifferential().getElement().each{ elt ->
         // check elements
+        // if binding is present then a reference valueset must exist
         if (elt.hasBinding()) {
           def binding = elt.getBinding()
           def ref = getReference(binding)
