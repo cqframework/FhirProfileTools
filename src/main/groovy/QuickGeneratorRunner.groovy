a192e5531990eccaba8222d211a238321bede849
Jason Mathews <mathews@mitre.org>
2015-10-15 09:56:44 -0400

    add fhirPublishDir field to prop file

--- src/main/groovy/QuickGeneratorRunner.groovy
+++ src/main/groovy/QuickGeneratorRunner.groovy
@@ -1,15 +1,19 @@
 // $Id: FhirHtmlGenerator.groovy,v 1.8 2015/05/21 09:59:42 mathews Exp $
 
 // location of published FHIR specification (aka FHIR website)
-File dir = new File("../build/publish")
+File publishDir = FhirUtils.getPublishDir()
 
-println "source: " + dir
-def gen = new QuickHtmlGenerator(dir)
+println "source: " + publishDir
+
+File qicoreDir = new File(publishDir, 'qicore')
+if (publishDir == null || !qicoreDir.isDirectory()) throw new Exception("QICORE profile directory not found")
+
+def gen = new QuickHtmlGenerator(publishDir)
 gen.devMode = true // disable for publishing and formal delivery
 gen.setup()
 gen.loadExtensions()
 
-new File(dir, 'qicore').eachFileMatch(~/.*profile.xml$/, { file ->
+qicoreDir.eachFileMatch(~/.*profile.xml$/, { file ->
   gen.process(file)
 })
 
